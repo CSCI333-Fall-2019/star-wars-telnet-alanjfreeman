199a46573a1dbd18fac5ffd1543f8c35d6c486eb
Alan Freeman <alan.freeman@principia.edu>
2019-09-25 22:56:21 -0500

    Added the project I made offline

---
+++ StarWarsTelnet/StarWarsTelnet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace StarWarsTelnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable initialization
+            Byte[] output = new Byte[1024];
+            string response = String.Empty;
+            string url = "towel.blinkenlights.nl";
+            int port = 23;
+
+            // Create TCP Client
+            var client = new TcpClient(url, port);
+            NetworkStream ns = client.GetStream();
+            Byte[] cmd = System.Text.Encoding.ASCII.GetBytes("\n");
+            ns.Write(cmd, 0, cmd.Length);
+
+            // Get output
+            Int32 bytes = ns.Read(output, 0, output.Length);
+
+            while (bytes > 0)
+            {
+                Console.Clear();
+                response = System.Text.Encoding.ASCII.GetString(output);
+                Console.WriteLine(response);
+                bytes = ns.Read(output, 0, output.Length);
+            }
+        }
+    }
+}
